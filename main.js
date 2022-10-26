4a7018f72e669fcaa5911c87ff5f014b46b57e8b
SebastianTabbia <tabbiasebastian@gmail.com>
2022-10-26 14:37:07 -0300

    cambios galerias

--- main.js
+++ main.js
@@ -109,14 +109,14 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener('DOMContentLoaded', function() {
 
     let imagenes = [
-        { img: './img/LPDA-LUNAPARK/DSC_0020-1 SOMBRAS.jpg' },
-        { img: './img/LPDA-LUNAPARK/DSC_0054-1.jpg' },
-        { img: './img/LPDA-LUNAPARK/DSC_0066.jpg' },
-        { img: './img/LPDA-LUNAPARK/DSC_0078-1.jpg' },
-        { img: './img/LPDA-LUNAPARK/DSC_0094.jpg' },
-        { img: './img/LPDA-LUNAPARK/DSC_0091-2.jpg' },
-        { img: './img/LPDA-LUNAPARK/DSC_0082 - SOMBRAS.jpg' },
-        { img: './img/WhatsApp Image 2022-10-18 at 14.13.01.jpeg' },
+        { img: './img/LPDA-LUNAPARK/1.jpg' },
+        { img: './img/LPDA-LUNAPARK/2.jpg' },
+        { img: './img/LPDA-LUNAPARK/3.jpg' },
+        { img: './img/LPDA-LUNAPARK/4.jpg' },
+        { img: './img/LPDA-LUNAPARK/5.jpg' },
+        { img: './img/LPDA-LUNAPARK/6.jpg' },
+        { img: './img/LPDA-LUNAPARK/7.jpg' },
+        { img: './img/LPDA-LUNAPARK/8.jpeg' },
     ];
 
     let contador = 0
@@ -166,5 +166,126 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 })
 
+//GALERIA 2//
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  let imagenes = [
+      { img: './img/LATAS/1.jpg' },
+      { img: './img/LATAS/2.jpg' },
+      { img: './img/LATAS/3.jpg' },
+      { img: './img/LATAS/4.jpg' },
+      { img: './img/LATAS/5.jpg' },
+      { img: './img/LATAS/6.jpg' },
+      { img: './img/LATAS/7.jpg' },
+      { img: './img/LATAS/8.jpg' },
+      { img: './img/LATAS/9.jpg' },
+      { img: './img/LATAS/10.jpg' },
+      { img: './img/LATAS/11.jpg' },
+  ];
+
+  let contador = 0
+  const contenedor = document.querySelector('.slideshow1');
+  const overlay = document.querySelector('.overlay1');
+  const galeria_imagenes = document.querySelectorAll('.galeria1 img');
+  const img_slideshows = document.querySelector('.slideshow1 img');
 
 
+  contenedor.addEventListener('click', function(event) {
+      let atras = contenedor.querySelector('.atras'),
+          adelante = contenedor.querySelector('.adelante'),
+          img = contenedor.querySelector('img'),
+          tgt = event.target
+      if (tgt == atras) {
+          if (contador > 0) {
+              img.src = imagenes[contador - 1].img
+              contador--
+          } else {
+              img.src = imagenes[imagenes.length - 1].img
+              contador = imagenes.length - 1
+          }
+      } else if (tgt == adelante) {
+          if (contador < imagenes.length - 1) {
+              img.src = imagenes[contador + 1].img
+              contador++
+          } else {
+              img.src = imagenes[0].img
+              contador = 0
+          }
+      }
+
+  })
+  Array.from(galeria_imagenes).forEach(img => {
+      img.addEventListener('click', event => {
+          const imagen_seleccionada = +(event.target.dataset.imgMostrar1)
+          img_slideshows.src = imagenes[imagen_seleccionada].img
+          contador = imagen_seleccionada
+          overlay.style.opacity = 1
+          overlay.style.visibility = 'visible'
+      })
+  })
+
+  var span = document.getElementsByClassName("btn-cerrar")[0];
+  span.onclick = function() {
+      overlay.style.visibility = "hidden"; 
+  }
+})
+
+//GALERIA 3//
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  let imagenes = [
+      { img: './img/Berlina/1.jpg' },
+      { img: './img/Berlina/2.jpg' },
+      { img: './img/Berlina/3.jpg' },
+      { img: './img/Berlina/4.jpg' },
+      { img: './img/Berlina/5.jpg' },
+  ];
+
+  let contador = 0
+  const contenedor = document.querySelector('.slideshow2');
+  const overlay = document.querySelector('.overlay2');
+  const galeria_imagenes = document.querySelectorAll('.galeria2 img');
+  const img_slideshows = document.querySelector('.slideshow2 img');
+
+
+  contenedor.addEventListener('click', function(event) {
+      let atras = contenedor.querySelector('.atras'),
+          adelante = contenedor.querySelector('.adelante'),
+          img = contenedor.querySelector('img'),
+          tgt = event.target
+      if (tgt == atras) {
+          if (contador > 0) {
+              img.src = imagenes[contador - 1].img
+              contador--
+          } else {
+              img.src = imagenes[imagenes.length - 1].img
+              contador = imagenes.length - 1
+          }
+      } else if (tgt == adelante) {
+          if (contador < imagenes.length - 1) {
+              img.src = imagenes[contador + 1].img
+              contador++
+          } else {
+              img.src = imagenes[0].img
+              contador = 0
+          }
+      }
+
+  })
+  Array.from(galeria_imagenes).forEach(img => {
+      img.addEventListener('click', event => {
+          const imagen_seleccionada = +(event.target.dataset.imgMostrar1)
+          img_slideshows.src = imagenes[imagen_seleccionada].img
+          contador = imagen_seleccionada
+          overlay.style.opacity = 1
+          overlay.style.visibility = 'visible'
+      })
+  })
+
+  var span = document.getElementsByClassName("btn-cerrar")[0];
+  span.onclick = function() {
+      overlay.style.visibility = "hidden"; 
+  }
+})
