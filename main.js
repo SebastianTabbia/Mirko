3d5a29f1980c6c34b778b0cb2c7e526c4605fccf
SebastianTabbia <tabbiasebastian@gmail.com>
2022-11-27 19:56:07 -0300

    contact about us
    se terminan esas secciones

--- main.js
+++ main.js
@@ -1165,3 +1165,70 @@ document.addEventListener('DOMContentLoaded', function() {
     span.onclick = function() {
         overlay.style.visibility = "hidden"; 
     }
+
+         //GALERIA 20//
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    let imagenes = [
+        { img: './img/CAMPAÑA VIOLENCIA DE GENERO/1.jpg' },
+        { img: './img/CAMPAÑA VIOLENCIA DE GENERO/2.jpg' },
+        { img: './img/CAMPAÑA VIOLENCIA DE GENERO/3.jpg' },
+        { img: './img/CAMPAÑA VIOLENCIA DE GENERO/4.jpg' },
+        { img: './img/CAMPAÑA VIOLENCIA DE GENERO/5.jpg' },
+        { img: './img/CAMPAÑA VIOLENCIA DE GENERO/6.jpg' },
+    ];
+  
+    let contador = 0
+    const contenedor = document.querySelector('.slideshow18');
+    const overlay = document.querySelector('.overlay18');
+    const galeria_imagenes = document.querySelectorAll('.galeria18 img');
+    const img_slideshows = document.querySelector('.slideshow18 img');
+  
+  
+    contenedor.addEventListener('click', function(event) {
+        let atras = contenedor.querySelector('.atras'),
+            adelante = contenedor.querySelector('.adelante'),
+            img = contenedor.querySelector('img'),
+            tgt = event.target
+        if (tgt == atras) {
+            if (contador > 0) {
+                img.src = imagenes[contador - 1].img
+                contador--
+            } else {
+                img.src = imagenes[imagenes.length - 1].img
+                contador = imagenes.length - 1
+            }
+        } else if (tgt == adelante) {
+            if (contador < imagenes.length - 1) {
+                img.src = imagenes[contador + 1].img
+                contador++
+            } else {
+                img.src = imagenes[0].img
+                contador = 0
+            }
+        }
+  
+    })
+    Array.from(galeria_imagenes).forEach(img => {
+        img.addEventListener('click', event => {
+            const imagen_seleccionada = +(event.target.dataset.imgMostrar1)
+            img_slideshows.src = imagenes[imagen_seleccionada].img
+            contador = imagen_seleccionada
+            overlay.style.opacity = 1
+            overlay.style.visibility = 'visible'
+        })
+    })
+  
+    var span = document.getElementsByClassName("btn-cerrar")[0];
+    span.onclick = function() {
+        overlay.style.visibility = "hidden"; 
+    }
+  })
+
+   
+  
+    var span = document.getElementsByClassName("btn-cerrar")[0];
+    span.onclick = function() {
+        overlay.style.visibility = "hidden"; 
+    }
